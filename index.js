27ac5e95479fe2a655272add92c888b35b1e812d
LetMCode <hoab2203718@student.ctu.edu.vn>
2024-11-09 16:11:57 +0700

    update

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ const port = process.env.PORT || 3000;
 app.use(morgan('combined'))
 
 // Đọc file api.json
-app.get('/api', (req, res) => {
+
+app.get('/api/page/:id', (req, res) => {
+    const { id } = req.params;
+
+    const filePath = path.join(__dirname, './product.json');
+    
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        return res.status(500).json({ error: 'Failed to read the JSON file' });
+      }
+  
+      const jsonData = JSON.parse(data);
+      res.json(jsonData.product[id].page);  // Trả về tất cả sản phẩm
+    });
+  });
+
+app.get('/api/cartUser', (req, res) => {
   const filePath = path.join(__dirname, './product.json'); // Đảm bảo đúng đường dẫn tới file api.json
 
   fs.readFile(filePath, 'utf8', (err, data) => {
@@ -17,7 +33,8 @@ app.get('/api', (req, res) => {
     }
 
     // Trả về dữ liệu JSON từ file
-    return res.json(JSON.parse(data));
+    const jsonData = JSON.parse(data);
+    return res.json(jsonData.cartUser);
   });
 });
 
