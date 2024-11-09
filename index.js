5ead677209f9e5f680fa640c176b1066a03f6032
LetMCode <hoab2203718@student.ctu.edu.vn>
2024-11-09 15:27:50 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Đọc file api.json
+app.get('/api', (req, res) => {
+  const filePath = path.join(__dirname, 'product.json'); // Đảm bảo đúng đường dẫn tới file api.json
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to read the JSON file' });
+    }
+
+    // Trả về dữ liệu JSON từ file
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
